53fd4fc14c937f9bb6e79d3b9ddc4c06cfd09a94
Clement Chan <clement.chan21@gmail.com>
2024-04-12 17:26:16 -0400

    Ran some more hyperparameter tuning. Started on streamlit app and need to add slides into repo.

---
+++ Streamlit/clinical_trial_app.py
@@ -0,0 +1,19 @@
+### Predicting Clinical Trial Terminations ###
+
+# conda install -c conda-forge streamlit
+
+# Importing Libraries
+import streamlit as st
+
+st.set_page_config(
+    page_title="Clinical Trial Terminations",
+    page_icon="👋"
+)
+
+st.write('# Predicting Clinical Trial Terminations')
+st.markdown('# Predicting Clinical Trial Terminations')
+st.header('Predicting Clinical Trial Terminations')
+st.title('Predicting Clinical Trial')
+
+st.sidebar.title("Navigation")
+st.sidebar.success("Select a page above.")
